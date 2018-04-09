f93713778a2f446e9226705559802a458f5274b7
Xavier de Gaye <xdegaye@gmail.com>
2018-04-09 16:55:42 +0200

    Add README.rst

--- README.rst
+++ README.rst
@@ -1,2 +1,13 @@
+**etcmaint [--version] {help,create,diff,sync,update} ...**
 
+An Arch Linux tool that uses git for the maintenance of /etc files.
 
+The /etc files installed or upgraded by Arch Linux packages are managed in the
+``etc`` branch of a git repository. The /etc files customized or created by
+the user are managed in the ``master`` branch. The upgraded package changes of
+the user-customized files are merged (actually cherry-picked) with the
+``update`` subcommand. Merge conflicts must be resolved and commited by the
+user. After a merge, the ``sync`` subcommand is used to retrofit the merged
+changes to /etc.
+
+Run ``etcmaint help <subcommand>`` to get help on a subcommand.
--- etcmaint/etcmaint.py
+++ etcmaint/etcmaint.py
@@ -1,14 +1,13 @@
 #! /bin/env python
 """An Arch Linux tool that uses git for the maintenance of /etc files.
 
-The /etc files installed or upgraded by Arch Linux packages are managed in
-the 'etc' branch of a git repository. The /etc files customized or created by
-the user are managed in the 'master' branch.
-
-The upgraded package changes of the user-customized files are merged (actually
-cherry-picked) with the 'update' subcommand. Merge conflicts must be resolved
-and commited by the user. After a merge, the 'sync' subcommand is used to
-retrofit those changes to /etc.
+The /etc files installed or upgraded by Arch Linux packages are managed in the
+``etc`` branch of a git repository. The /etc files customized or created by
+the user are managed in the ``master`` branch. The upgraded package changes of
+the user-customized files are merged (actually cherry-picked) with the
+``update`` subcommand. Merge conflicts must be resolved and commited by the
+user. After a merge, the ``sync`` subcommand is used to retrofit the merged
+changes to /etc.
 """
 
 import sys
